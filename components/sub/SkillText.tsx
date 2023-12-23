8b908921a6e6ecd35442c21f1c3e672e50f00465
punitkr03 <tagpunitkumar@gmail.com>
2023-12-24 02:23:45 +0530

    fix

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -16,8 +16,8 @@ const SkillText = () => {
         className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
       >
         <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-        <h1 className="Welcome-text text-[13px]">
-          Think better with Next js 13
+        <h1 className="Welcome-text text-[15px]">
+          Think better with Next.js 14
         </h1>
       </motion.div>
       <motion.div
