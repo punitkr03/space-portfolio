913f2f31e20030c4092f3feeb0faa454a3bf65c7
punitkr03 <tagpunitkumar@gmail.com>
2023-12-24 02:11:16 +0530

    fix

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -33,7 +33,7 @@ const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Hi, I'm
+            Hi, I&apos;m
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
               Punit Kumar{" "}
