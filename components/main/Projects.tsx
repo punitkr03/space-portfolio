bd90d56b664c7b04a24b4bcaade48d18cebb41d4
punitkr03 <tagpunitkumar@gmail.com>
2023-12-29 19:51:09 +0530

    Final links updated

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -8,26 +8,35 @@ const Projects = () => {
     <div className="z-[20] flex flex-col items-center justify-center py-20">
       <Element name="projects"></Element>
       <h1
-        className="josefin text-5xl md:text-6xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20"
+        className="josefin text-center text-5xl md:text-6xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20"
         id="projects"
       >
-        {`< My Projects />`}
+        {`< Featured Projects />`}
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         <ProjectCard
-          src="/NextWebsite.png"
-          title="Modern Next.js Portfolio"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/Docubuddy.webm"
+          imgsrc={false}
+          githubURL="https://github.com/punitkr03/docubuddy"
+          siteURL="https://docubuddy.vercel.app/"
+          title="DocuBuddy"
+          description="DocuBuddy allows you to have conversations with any PDF document. Simply upload your file and start asking questions right away."
         />
         <ProjectCard
-          src="/CardImage.png"
-          title="Interactive Website Cards"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
-        <ProjectCard
-          src="/SpaceWebsite.png"
+          src="/space-portfolio.png"
+          imgsrc={true}
+          githubURL="https://github.com/punitkr03/space-portfolio"
+          siteURL="https://punitkumar.vercel.app/"
           title="Space Themed Website"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          description="Space themed portfolio which showcases my web development skills and also my interest in Space Exploration."
+        />
+        <ProjectCard
+          src="/lazy-app.png"
+          imgsrc={true}
+          title="Lazy App"
+          githubURL="https://github.com/punitkr03/Lazy-App"
+          siteURL="https://lazy-app.netlify.app/"
+          description="A micro-gig platform built to cater to gigs created around you and in your locality. Users can find gigs and also create gigs."
         />
       </div>
     </div>
--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -1,15 +1,60 @@
 import Image from "next/image";
-import React from "react";
+import { RxExternalLink, RxGithubLogo } from "react-icons/rx";
 
 interface Props {
   src: string;
+  imgsrc: boolean;
   title: string;
   description: string;
+  githubURL: string;
+  siteURL: string;
 }
 
-const ProjectCard = ({ src, title, description }: Props) => {
-  return (
-    <div className="hover:scale-110 cursor-pointer relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
+const ProjectCard = ({
+  src,
+  imgsrc,
+  title,
+  description,
+  githubURL,
+  siteURL,
+}: Props) => {
+  return !imgsrc ? (
+    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
+      <video
+        autoPlay
+        muted
+        loop
+        width={1000}
+        height={1000}
+        className="object-contain"
+      >
+        <source src={src} type="video/webm" />
+      </video>
+
+      <div className="relative p-4">
+        <h1 className="text-2xl font-semibold text-white">{title}</h1>
+        <p className="mt-2 text-gray-300">{description}</p>
+      </div>
+
+      <div className="flex items-center justify-center gap-10">
+        <a
+          href={githubURL}
+          target="_blank"
+          className="flex items-center justify-center my-[15px] cursor-pointer z-[10] text-white text-2xl"
+        >
+          <RxGithubLogo />
+        </a>
+        <a
+          href={siteURL}
+          target="_blank"
+          className="flex items-center justify-center my-[15px] cursor-pointer z-[10] text-white text-2xl"
+        >
+          <RxExternalLink />
+        </a>
+      </div>
+    </div>
+  ) : (
+    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
       <Image
         src={src}
         alt={title}
@@ -21,6 +66,22 @@ const ProjectCard = ({ src, title, description }: Props) => {
       <div className="relative p-4">
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
         <p className="mt-2 text-gray-300">{description}</p>
+      </div>
+      <div className="flex items-center justify-center gap-10">
+        <a
+          href={githubURL}
+          target="_blank"
+          className="flex items-center justify-center my-[15px] cursor-pointer z-[10] text-white text-2xl"
+        >
+          <RxGithubLogo />
+        </a>
+        <a
+          href={siteURL}
+          target="_blank"
+          className="flex items-center justify-center my-[15px] cursor-pointer z-[10] text-white text-2xl"
+        >
+          <RxExternalLink />
+        </a>
       </div>
     </div>
   );
